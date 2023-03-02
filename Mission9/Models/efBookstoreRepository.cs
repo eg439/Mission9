c38ca483f94398d40a7477293d6c4ff3947aa40c
Eliza Greer <eg439@byu.edu>
2023-03-01 22:58:54 -0700

    Add project files.

---
+++ Mission9/Models/efBookstoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9.Models
+{
+    public class efBookstoreRepository : IBookstoreRepository
+    {
+
+        private BookstoreContext context { get; set; }
+
+        public efBookstoreRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Books> Books => context.Books;
+    }
+}
